28a4e5bfd6cc1ece718ddedd63cc2307cd095322
Tyurnin <t_svjatoslav@mail.ru>
2023-01-12 14:22:20 +0300

    3 dz

---
+++ lesson_3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main()
+{
+    int pin1,pin2;
+    printf("pin1=");
+    scanf("%i",&pin1);
+    printf("pin2=");
+    scanf("%i",&pin2);
+
+    if
+    (((pin1==123)&&(pin2==321))||
+    ((pin1==456)&&(pin2==654))||
+    ((pin1==789)&&(pin2==987)))
+    {
+        printf("OK");
+    }
+   
+   else
+    {
+        printf("Error");
+    }
+    
+}
